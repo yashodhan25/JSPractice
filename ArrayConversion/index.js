fdb75e303755d407c3c41a9bf47d8413f82af35b
yashodhan <techlabrcl@gmail.com>
2022-03-12 15:54:34 +0530

    Update

--- ArrayConversion/index.js
+++ ArrayConversion/index.js
@@ -1,3 +1,26 @@
+/*
+let question = [
+    {'id': 1, 'date': 01/08/2010, 'Message':'Hi !'},
+    {'id': 2, 'date': 01/08/2010, 'Message':'Hello !'},
+    {'id': 3, 'date': 08/12/2010, 'Message':'Hey ?'},
+    {'id': 4, 'date': 18/02/2012, 'Message':'Yeah !'},
+    {'id': 5, 'date': 18/02/2012, 'Message':'Hii !'},
+    {'id': 6, 'date': 30/09/2018, 'Message':'How r u ?'},
+    {'id': 7, 'date': 30/09/2018, 'Message':'fine ?'},
+    {'id': 8, 'date': 30/09/2018, 'Message':'About You ?'}
+];
+
+// Output
+let ans = [
+    {'date':01/08/2010, 'data':[{'id':1,'message':'Hi !'},{'id': 2,'Message':'Hello !'}]},
+    {'date':08/12/2010, 'date':[{'id':3,'message':'Hey ?'}]},
+    {'date':18/02/2012, 'date':[{'id':4,'message':'Yeah !'},{'id':5,'message':'Hii !'}]},
+    {'date':30/09/2018, 'date':[{'id':6,'message':'How r u ?'},{'id':7,'message':'fine ?'},{'id':8,'message':'About You ?'}]}
+]
+*/
+
+/*=================================== Solution ===================================*/
+
 let givenArray = [
     {'id': 1, 'date': 01/08/2010, 'Message':'Hi !'},
     {'id': 2, 'date': 01/08/2010, 'Message':'Hello !'},
@@ -27,23 +50,5 @@ function formatted(date,data){
     formattedData.push({date: date, data: data})
 }
 console.log(formattedData)
-/*
-let question = [
-    {'id': 1, 'date': 01/08/2010, 'Message':'Hi !'},
-    {'id': 2, 'date': 01/08/2010, 'Message':'Hello !'},
-    {'id': 3, 'date': 08/12/2010, 'Message':'Hey ?'},
-    {'id': 4, 'date': 18/02/2012, 'Message':'Yeah !'},
-    {'id': 5, 'date': 18/02/2012, 'Message':'Hii !'},
-    {'id': 6, 'date': 30/09/2018, 'Message':'How r u ?'},
-    {'id': 7, 'date': 30/09/2018, 'Message':'fine ?'},
-    {'id': 8, 'date': 30/09/2018, 'Message':'About You ?'}
-];
 
-// Output
-let ans = [
-    {'date':01/08/2010, 'data':[{'id':1,'message':'Hi !'},{'id': 2,'Message':'Hello !'}]},
-    {'date':08/12/2010, 'date':[{'id':3,'message':'Hey ?'}]},
-    {'date':18/02/2012, 'date':[{'id':4,'message':'Yeah !'},{'id':5,'message':'Hii !'}]},
-    {'date':30/09/2018, 'date':[{'id':6,'message':'How r u ?'},{'id':7,'message':'fine ?'},{'id':8,'message':'About You ?'}]}
-]
-*/
+/*=================================== End ===================================*/
